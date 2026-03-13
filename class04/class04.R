19f7a950b9d33446270649ac37747f96e22c7b22
Kate Ruiz <k2ruiz@ucsd.edu>
2026-03-13 11:43:08 -0700

    add c4

---
+++ class04/class04.R
@@ -0,0 +1,8 @@
+#' ---
+#' title: "Class 04"
+#' author: "Kate Ruiz"
+#' date: "January 16th, 2026"
+#' ---
+x <- 1:50
+plot(x)
+plot (x, sin(x), typ="l", col="blue", lwd=3, xlab="Is it lunch time yet")
